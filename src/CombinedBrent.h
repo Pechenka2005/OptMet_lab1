2a0b26b99166c17229294de99329d1fce0d9a8e1
Pechenka2005 <serg-20012001@mail.ru>
2021-03-04 00:03:14 +0300

    added title page

---
+++ src/CombinedBrent.h
@@ -0,0 +1,20 @@
+//
+// Created by Сергей on 03.03.2021.
+//
+
+#ifndef INC_1_LAB_COMBINEDBRENT_H
+#define INC_1_LAB_COMBINEDBRENT_H
+
+#include "Function.h"
+
+class CombinedBrent {
+private:
+    double a, c, x, w, v, u;
+public:
+    CombinedBrent(double a, double c) {
+
+    }
+};
+
+
+#endif //INC_1_LAB_COMBINEDBRENT_H
