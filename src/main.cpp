30a73633580276ce7688aba0fc3fa7d27d85aa4f
Pechenka2005 <serg-20012001@mail.ru>
2021-03-05 02:54:16 +0300

    change main

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,18 @@
 #include "Dichotomy.h"
 #include "Parabola.h"
 #include "GoldenRatio.h"
+#include "CombinedBrent.h"
 
 using namespace std;
 
 int main() {
     Dichotomy res_d(0.5, 4, 0.00001, 0.0000001);
     cout << "Method Dichotomy: " << res_d.evaluate() << endl;
-    Parabola res_p(0.5, 4, 0.00001);
+    Parabola res_p(0.5, 4, 0.0000001);
     cout << "The parabola method: " << res_p.evaluate() << endl;
     GoldenRatio res_gr(0.5, 4, 0.00000001);
     cout << "The golder ratio method: " << res_gr.evaluate() << endl;
+    CombinedBrent res_comBrant(0.5, 4, 0.0000001);
+    cout << "The Combined Brent method: " << res_comBrant.evaluate() << endl;
     return 0;
 }
