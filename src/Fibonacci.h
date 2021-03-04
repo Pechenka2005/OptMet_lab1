9625f46a4eef59322a8889f18e3ddc497195edc9
SoKoL <sokolov-19.07@yandex.ru>
2021-03-05 02:40:41 +0300

    Fibonacci work

--- src/Fibonacci.h
+++ src/Fibonacci.h
@@ -8,8 +8,21 @@
 
 class Fibonacci : Function {
 private:
-
+    double a, b, eps;
+    int maxNumberOfFib = 50;
+    int fib[100]{};
+    int find_n();
+    void find_fibonacci();
+    double find_x1(double a, double b, int n) const;
+    double find_x2(double a, double b, int n) const;
 public:
+    Fibonacci(double a, double b, double eps) {
+        this->a = a;
+        this->b = b;
+        this->eps = eps;
+    }
+    void setValue(double a, double b, double eps);
+    double evaluate();
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Dichotomy.h"
 #include "Parabola.h"
 #include "GoldenRatio.h"
+#include "Fibonacci.h"
 
 using namespace std;
 
@@ -12,5 +13,7 @@ int main() {
     cout << "The parabola method: " << res_p.evaluate() << endl;
     GoldenRatio res_gr(0.5, 4, 0.00000001);
     cout << "The golder ratio method: " << res_gr.evaluate() << endl;
+    Fibonacci res_f(0.5, 4, 0.0000001);
+    cout << "The fibonacci method: " << res_f.evaluate() << endl;
     return 0;
 }
