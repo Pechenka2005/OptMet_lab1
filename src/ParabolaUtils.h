599961443276ba1ee8698ce36660172c8a16cdb2
Pechenka2005 <serg-20012001@mail.ru>
2021-03-18 16:09:19 +0300

    трахнул

--- src/ParabolaUtils.h
+++ src/ParabolaUtils.h
@@ -11,11 +11,8 @@ protected:
 
     static double find_a_2(double x1, double x2, double x3);
 
-<<<<<<< HEAD
     static double find_x_n(double x1, double x2, double f1, double f2);
-=======
-    static double find_x_n(double x1, double x2, double x3);
->>>>>>> 1d4da41815e972943625a87bb957295f9f557b68
+
 };
 
 #endif //INC_1_LAB_PARABOLAUTILS_H
